678b10b75f43bd5965cadf5644cf79f4f10bdd50
Bruno <brunohenrique0665@gmail.com>
2024-11-25 21:32:49 -0300

    implementing camera that follows the player and adding a bigger map

--- src/main/java/entity/Entity.java
+++ src/main/java/entity/Entity.java
@@ -4,7 +4,7 @@ import java.awt.image.BufferedImage;
 
 public class Entity {
 
-    public int x, y;
+    public int worldX, worldY;
     public int speed;
 
     // Describes an image with an accessible buffer of image data. used to store image files.

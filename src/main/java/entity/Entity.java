fa5bc89fbfb71d9279f53f33d8df81f4263167a7
Bruno <brunohenrique0665@gmail.com>
2025-01-18 22:29:38 -0300

    fix: particle generator

--- src/main/java/entity/Entity.java
+++ src/main/java/entity/Entity.java
@@ -332,10 +332,10 @@ public class Entity {
         int speed = generator.getParticleSpeed();
         int maxLife = generator.getParticleMaxLife();
 
-        Particle particle = new Particle(gp, generator, color, size, speed, maxLife, -1, -1);
-        Particle particle2 = new Particle(gp, generator, color, size, speed, maxLife, 1, -1);
-        Particle particle3 = new Particle(gp, generator, color, size, speed, maxLife, -1, 1);
-        Particle particle4 = new Particle(gp, generator, color, size, speed, maxLife, 1, 1);
+        Particle particle = new Particle(gp, target, color, size, speed, maxLife, -1, -1);
+        Particle particle2 = new Particle(gp, target, color, size, speed, maxLife, 1, -1);
+        Particle particle3 = new Particle(gp, target, color, size, speed, maxLife, -1, 1);
+        Particle particle4 = new Particle(gp, target, color, size, speed, maxLife, 1, 1);
         gp.particleList.add(particle);
         gp.particleList.add(particle2);
         gp.particleList.add(particle3);
